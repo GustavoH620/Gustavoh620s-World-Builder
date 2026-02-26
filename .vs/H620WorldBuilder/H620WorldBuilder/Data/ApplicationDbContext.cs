6c04122e22c8a9979c1f82747540c2aace3d00df
Gustavo C <137294905+GustavoH620@users.noreply.github.com>
2026-02-26 20:01:57 -0300

    Configuração de projeto

---
+++ .vs/H620WorldBuilder/H620WorldBuilder/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace H620WorldBuilder.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+    }
+}
